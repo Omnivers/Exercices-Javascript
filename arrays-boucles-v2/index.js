338ae76a3c61208f8dc66a28e172f8b3072c1348
Omnivers <Twersimor.stock@outlook.com>
2022-06-22 14:45:43 +0200

    2/15 exercice  V2

---
+++ arrays-boucles-v2/index.js
@@ -0,0 +1,27 @@
+// 01 - Nombre pairs
+nombreMax=47;
+nombreMin=11;
+somme=0;
+for (i=nombreMin;i<nombreMax;i++){
+    if(i%2==0){
+        console.log(`${i} Est un nombre pair`)
+        somme=somme+i;
+    }
+}
+console.log(somme);
+
+// 02 - Nombre impairs
+
+nombreMax=588;
+nombreMin=109;
+for (i=nombreMin;i<nombreMax;i++){
+    if(i%2!=0){
+        console.log(`${i} Est un nombre impair`)
+        somme=somme+i;
+    }
+}
+console.log(somme);
+
+// 03 - Puissance et racine carrée
+
+
