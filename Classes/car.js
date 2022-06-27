1a151ae15ee0c1dc6c49f719f5b0fcb2a22078cd
Omnivers <Twersimor.stock@outlook.com>
2022-06-27 16:09:03 +0200

    Class exercices 3/3

--- Classes/car.js
+++ Classes/car.js
@@ -31,3 +31,57 @@ console.log(mercedes);
 mercedes.brake(250);
 console.log(mercedes);
 
+// 2 - TV
+
+class TV{
+    constructor(brand,channel,volume){
+        this.brand=brand;
+        this.channel=1;
+        this.volume=50;
+    }
+    addVolume(num){
+        let newVolume=this.volume+num;
+        if(newVolume>0 && newVolume<100){
+            this.volume=newVolume;
+        }
+    }
+    changeChannel(num){
+        if(num<50 && num>0){
+            this.channel=num;
+        }
+    }
+    reset(){
+        this.channel=1;
+        this.volume=50;
+    }
+    describe(){
+        console.log(`Le model de la télévision est ${brand}, le volume est de ${volume}, la chaine est ${channel}`);
+    }
+}
+const samsung=new TV('samsung',1,30);
+samsung.addVolume(30);
+samsung.changeChannel(34);
+samsung.reset();
+console.log(samsung);
+
+// 3 - Video games;
+
+let gender=null;
+class Png{
+    constructor(firstName,lastName,sexe){
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.sexe=sexe;
+    }
+    player(){
+        if(this.sexe==="male"){
+            gender="he's"
+        }
+        else{
+            gender="she's"
+        }
+        console.log(`The character you choosed is ${this.firstName} ${this.lastName} ${gender} a ${this.sexe}`);
+    }
+}
+const myplayer= new Png("Jad","Aroussi","male");
+myplayer.player();
