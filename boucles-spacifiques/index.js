82c10234340443b2877d8677bc9faa2f4cd633d7
Omnivers <Twersimor.stock@outlook.com>
2022-06-30 23:46:52 +0200

    Exercices JS 05/09

--- boucles-spacifiques/index.js
+++ boucles-spacifiques/index.js
@@ -40,3 +40,46 @@
 //     return i%2===0;
 // })
 // console.log(even);
+
+// 05 - Cakes 
+
+// let Cakes = [
+// 	{
+// 		name: "cake",
+// 		flavor: "vanilla",
+// 		status: "available"
+// 	},
+// 	{
+// 		name: "brownie",
+// 		flavor: "chocolate",
+// 		status: "available"
+// 	},
+// 	{
+// 		name: "pie",
+// 		flavor: "strawberry",
+// 		status: "available"
+// 	},
+// 	{
+// 		name: "muffin",
+// 		flavor: "pistachio",
+// 		status: "available"
+// 	},
+// 	{
+// 		name: "donut",
+// 		flavor: "chocolate",
+// 		status: "available"
+// 	},
+// ]
+
+// let chocolated=Cakes.filter((cake)=>{
+//     return cake.flavor==="chocolate";
+// })
+// const soldOut=chocolated.map((cake)=>{
+//     return {
+//         name:cake.name,
+//         flavor:cake.flavor,
+//         status:"Sold Out !"
+//     }
+// });
+// console.log(soldOut);
+
