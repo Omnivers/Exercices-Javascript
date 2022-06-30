246b8b747e1df87f4736704d189f369a885e37d0
Omnivers <Twersimor.stock@outlook.com>
2022-06-30 23:26:32 +0200

    exercices JS 2/9

---
+++ boucles-spacifiques/index.js
@@ -0,0 +1,26 @@
+// 01 - Map double 
+// let array=[1,2,3,4,5];
+// const double=array.map((number)=>{
+//     return number*2;
+// })
+// console.log(double);
+
+
+// 02 - Map Names 
+
+// const longNames=[
+// 	{
+// 		firstName: "Jane",
+// 		lastName: "Doe"
+// 	},
+// 	{
+// 		firstName: "John",
+// 		lastName: "Smith"
+// 	}
+// ];
+// const shortNames=longNames.map((name)=>{
+//     return {
+//         name:name.firstName +" "+ name.lastName
+//     };
+// })
+// console.log(shortNames);
